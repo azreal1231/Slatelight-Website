9b344ac114bc5bf746cdacf2c3105b09974a9d42
Azreal <52235517+azreal1231@users.noreply.github.com>
2024-01-11 00:41:48 +0200

    ? save point

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import './kak.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [pageMounted, setPageMounted] = useState(false)
 
-  return (
-    <>
-  <nav className="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
-    {/* <a class="navbar-brand" href="#">Slatelight</a> */}
+useEffect(() => {
+  if(!pageMounted){
+    setPageMounted(true)
+    doRetainerThing()
+    doAddonThing()
+  }
+}, [pageMounted])
+
+function doRetainerThing(){
+    const cards = document.querySelectorAll('.retainer-card');
+  
+    cards.forEach(card => {
+      card.addEventListener('mouseenter', function() {
+        // Increase the blur of the hovered card
+        this.style.backdropFilter = 'blur(10px)';
+        this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
+
+  
+        // Reset the blur of all other cards
+        cards.forEach(otherCard => {
+          if (otherCard !== this) {
+            otherCard.style.backdropFilter = 'blur(0px)';
+            otherCard.style.opacity = '0.5';
+          }
+        });
+      });
+  
+      card.addEventListener('mouseleave', function() {
+        // Reset the gradient and opacity when the mouse leaves the card
+        this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
+        cards.forEach(otherCard => {
+          otherCard.style.opacity = '1';
+        });
+      });
+    });
+}
+
+function doAddonThing(){
+  const cards = document.querySelectorAll('.addons-card');
+
+  cards.forEach(card => {
+    card.addEventListener('mouseenter', function() {
+      // Increase the blur of the hovered card
+      this.style.backdropFilter = 'blur(10px)';
+      this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
+
+
+      // Reset the blur of all other cards
+      cards.forEach(otherCard => {
+        if (otherCard !== this) {
+          otherCard.style.backdropFilter = 'blur(0px)';
+          otherCard.style.opacity = '0.5';
+        }
+      });
+    });
+
+    card.addEventListener('mouseleave', function() {
+      // Reset the gradient and opacity when the mouse leaves the card
+      this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
+      cards.forEach(otherCard => {
+        otherCard.style.opacity = '1';
+      });
+    });
+  });
+}
+
+return <>
+<nav className="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+    {/* <a className="navbar-brand" href="#">Slatelight</a> */}
     <img style={{ width: 200 }} src="/imgs/Logo.png" alt="" />
     <button
       className="navbar-toggler"
@@ -45,159 +111,72 @@ function App() {
       </form>
     </div>
   </nav>
-  {/* Navbar */}
-  {/* Full Page Intro */}
-  <div
-    className="view"
-    style={{
-      backgroundImage: "url(imgs/top_background.jpg)",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover"
-    }}
-  >
-    {/* <div class="view" style="background-image: url(/static/index_page/img/stock-photo-web-or-application-development-business-and-technology-concept-programmer-man-software-developer-1157354179.jpg); background-position: center; background-repeat: no-repeat; background-size: cover;"> */}
-    {/* Mask & flexbox options*/}
-    <div className="mask rgba-black-light d-flex justify-content-center align-items-center">
-      {/* Content */}
-      <div className="jumbotron">
-        <div className="container">
-          <h1 className="display-4">
-            <b>“Websites promote you 24/7: No employee will do that.”</b>
-          </h1>
-          <p className="lead" style={{ fontSize: 30 }}>
-            Paul Cookson
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <section className="About  text-sm-center text-center pt-4" align="center">
-    <a name="About" />
-    <h2 style={{ color: "azure" }}>About</h2>
-    <p className="text-sm-center">
-      Slatelight | Web development is focused on providing new modern,
-      <br />
-      simple effective website's with the ability to make online
-      <label style={{ textDecoration: "underline" }}>bookings/payments</label>
-      for your bussiness.
-      <br />
-    </p>
-  </section>
-  <section className="About  text-sm-center text-center pb-3" align="center">
-    <a name="Services" />
-    <h2 style={{ color: "azure" }}>Services</h2>
+  <div id="stars"></div>
+  <div id="stars2"></div>
+  <div id="stars3"></div>
+  {/* TODO */}
+  {/* create me a new div that i can use as my bodu that floats ontop of my main body but is transparent and i can add content where i can also scroll down */}
+  <div className="floating-content">
     <div className="container">
-      <div className="card-deck">
-        <div
-          className="card border border-secondary rounded"
-          style={{ background: "transparent" }}
-        >
-          {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-          <div className="card-body">
-            <h5 className="card-title">Business Automation</h5>
-            <p className="card-text"></p>
-            <p className="text-info" style={{ fontSize: "medium" }}>
-              {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga eius quo porro corrupti eligendi. Laborum excepturi sint ad a veritatis molestias doloribus totam labore nulla quam aut, tempora iure! */}
-            </p>
-            <ul className="" align="left">
-              <li>Customer relationship management 'CRM'</li>
-              <li>Custom business management platforms</li>
-              <li>Tracking and reporting</li>
-              <li>Online Payments</li>
-            </ul>
-            <p />
-            {/*                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>*/}
+      <div className="row">
+        <div className="col-sm-3" style={{ height: '200px' }}>
+          <div className="retainer-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
           </div>
         </div>
-        <div
-          className="card border border-secondary rounded"
-          style={{ background: "transparent" }}
-        >
-          {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-          <div className="card-body">
-            <h5 className="card-title">Website's</h5>
-            <p className="card-text text-info"></p>
-            <p className="text-info" style={{ fontSize: "medium" }}>
-              {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga eius quo porro corrupti eligendi. Laborum excepturi sint ad a veritatis molestias doloribus totam labore nulla quam aut, tempora iure! */}
-            </p>
-            <ul align="left">
-              <li>Website Design/Development</li>
-              <li>Domain Registration/Management</li>
-            </ul>
-            <p />
-            {/*                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>*/}
+        <div className="col-sm-3" style={{'height': '200px'}}>
+          <div className="retainer-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
           </div>
         </div>
-        <div
-          className="card border border-secondary rounded"
-          style={{ background: "transparent" }}
-        >
-          {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-          <div className="card-body">
-            <h5 className="card-title">Android</h5>
-            <p className="text-info" style={{ fontSize: "medium" }}>
-              {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga eius quo porro corrupti eligendi. Laborum excepturi sint ad a veritatis molestias doloribus totam labore nulla quam aut, tempora iure! */}
-            </p>
-            <div align="center">
-              <ul align="left">
-                <li>Android For Business</li>
-                <li>Custom Point Of Sale Apps</li>
-              </ul>
-            </div>
+        <div className="col-sm-3" style={{'height': '200px'}}>
+          <div className="retainer-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+        </div>
+        <div className="col-sm-3" style={{'height': '200px'}}>
+          <div className="retainer-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
           </div>
         </div>
       </div>
-      <div className="card-deck pt-4">
-        <div
-          className="card border border-secondary rounded"
-          style={{ background: "transparent" }}
-        >
-          {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-          <div className="card-body">
-            <h5 className="card-title">Hosting</h5>
-            {/* <p class="text-info" style="font-size: medium;">
-            Having all your information on the cloud is very importent, Slatelight uses <a target="_blank" href="https://www.digitalocean.com/"><B>Digital Ocean</B></a>  for all our projects
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum, molestias animi minima iusto quae quasi tempore eaque eum, impedit sequi, nobis eligendi minus neque. Fugiat ipsum rem quisquam molestiae.
-          </p> */}
-            <ul align="left">
-              <li>Website Hosting</li>
-              <li>DataBase Hosting</li>
-              <li>CRM Hosting</li>
-            </ul>
+
+      <div className="row d-flex justify-content-center" style={{marginTop: '100px'}}>
+        <div className="col-sm-3" style={{ height: '200px' }}>
+          <div className="addons-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+        </div>
+        <div className="col-sm-3" style={{'height': '200px'}}>
+          <div className="addons-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+          </div>
+        </div>
+        <div className="col-sm-3" style={{'height': '200px'}}>
+          <div className="addons-card">
+            <h3>Title</h3>
+            <h5>Subject</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  <section className="parallax">
-    {/*this will be that img that moves in the background*/}
-    <div className="parallax-inner"></div>
-  </section>
-  <section className="Contact">
-    <a name="Contact" />
-    <h2 style={{ color: "azure" }}>Contact</h2>
-    {/* <p>My Name <br class="break"> <a href="mailto:edwardjack91@gmail.com"> edwardjack91@gmail.com</a> */}
-    <br className="break" />
-    <p>
-      <a style={{ color: "azure" }} href="tel:0764169752">
-        27-76-416-9752
-      </a>
-      <br className="break" />
-    </p>
-    <p>
-      <a style={{ color: "azure" }} href="mailto:info@slatelight.co.za">
-        info@slatelight.co.za
-      </a>
-      {/*        `<p>12 Street Road <br class="break">Western Cape  <br class="break">Cape Town<br class="break"> 19000</p>*/}
-    </p>
-  </section>
-  <div className="footer position-absolute w-100 pt-2">
-    <p>©Slatelight.co.za 2K20</p>
+    {/* Add your content here */}
   </div>
 </>
 
-  )
 }
 
 export default App
