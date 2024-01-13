25ae314bc7e8b514c23f9f9f3bc792e15ff9cc4e
Azreal <52235517+azreal1231@users.noreply.github.com>
2024-01-13 15:24:53 +0200

    ? save point

--- src/App.jsx
+++ src/App.jsx
@@ -25,65 +25,6 @@ function generateStars() {
   return stars.slice(0, -2);
 }
 
-
-function doRetainerThing(){
-    const cards = document.querySelectorAll('.retainer-card');
-  
-    cards.forEach(card => {
-      card.addEventListener('mouseenter', function() {
-        // Increase the blur of the hovered card
-        this.style.backdropFilter = 'blur(10px)';
-        this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
-
-  
-        // Reset the blur of all other cards
-        cards.forEach(otherCard => {
-          if (otherCard !== this) {
-            otherCard.style.backdropFilter = 'blur(0px)';
-            otherCard.style.opacity = '0.3';
-          }
-        });
-      });
-  
-      card.addEventListener('mouseleave', function() {
-        // Reset the gradient and opacity when the mouse leaves the card
-        this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
-        cards.forEach(otherCard => {
-          otherCard.style.opacity = '1';
-        });
-      });
-    });
-}
-
-function doAddonThing(){
-  const cards = document.querySelectorAll('.addons-card');
-
-  cards.forEach(card => {
-    card.addEventListener('mouseenter', function() {
-      // Increase the blur of the hovered card
-      this.style.backdropFilter = 'blur(10px)';
-      this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
-
-
-      // Reset the blur of all other cards
-      cards.forEach(otherCard => {
-        if (otherCard !== this) {
-          otherCard.style.backdropFilter = 'blur(0px)';
-          otherCard.style.opacity = '0.3';
-        }
-      });
-    });
-
-    card.addEventListener('mouseleave', function() {
-      // Reset the gradient and opacity when the mouse leaves the card
-      this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
-      cards.forEach(otherCard => {
-        otherCard.style.opacity = '1';
-      });
-    });
-  });
-}
-
 return <>
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
--- src/comps/addons.jsx
+++ src/comps/addons.jsx
@@ -1,6 +1,37 @@
 import { addons } from "../utils/constants"
 
 const AddonsComp = () => {
+
+
+function doAddonThing(){
+    const cards = document.querySelectorAll('.addons-card');
+    
+    cards.forEach(card => {
+        card.addEventListener('mouseenter', function() {
+        // Increase the blur of the hovered card
+        this.style.backdropFilter = 'blur(10px)';
+        this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
+    
+    
+        // Reset the blur of all other cards
+        cards.forEach(otherCard => {
+            if (otherCard !== this) {
+            otherCard.style.backdropFilter = 'blur(0px)';
+            otherCard.style.opacity = '0.3';
+            }
+        });
+        });
+    
+        card.addEventListener('mouseleave', function() {
+        // Reset the gradient and opacity when the mouse leaves the card
+        this.style.backgroundImage = 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0) 50%, rgba(0, 0, 0, 0.0) 50%)';
+        cards.forEach(otherCard => {
+            otherCard.style.opacity = '1';
+        });
+        });
+    });
+}
+
 return <>
 <div className="row d-flex justify-content-center" style={{marginTop: '100px'}}>
     {addons.map((_x, _index) => 
