0468f0e22b7fa593a7286cd63d0a18ec7f3db524
Azreal <52235517+azreal1231@users.noreply.github.com>
2024-01-15 23:34:33 +0200

    ? removed bs js dep

--- src/comps/navbar.jsx
+++ src/comps/navbar.jsx
@@ -1,32 +1,40 @@
+import React, { useState } from 'react';
+
 const NavBar = () => {
-return <>
-<nav className="navbar navbar-expand-lg my-navbar">
-    <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-            <img style={{ maxHeight: '70px', maxWidth: '100%', height: 'auto' }} src="/imgs/Logo.png" alt="logo" className="d-inline-block align-text-top" />
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#about">About</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#references">References</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#packages">Packages</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#partners">Partners</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-</>
+    // State to manage the collapse of the navbar
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    // Function to toggle the navbar collapse state
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+    return (
+        <nav className="navbar navbar-expand-lg my-navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    <img style={{ maxHeight: '70px', maxWidth: '100%', height: 'auto' }} src="/imgs/Logo.png" alt="logo" className="d-inline-block align-text-top" />
+                </a>
+                <button className="navbar-toggler" type="button" onClick={handleNavCollapse} aria-controls="navbarNav" aria-expanded={!isNavCollapsed} aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-end`} id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#about" onClick={() => setIsNavCollapsed(true)}>About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#references" onClick={() => setIsNavCollapsed(true)}>References</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#packages" onClick={() => setIsNavCollapsed(true)}>Packages</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active text-white fs-5 text-center" aria-current="page" href="#partners" onClick={() => setIsNavCollapsed(true)}>Partners</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
