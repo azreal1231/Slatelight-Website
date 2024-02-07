a8ff84ed36df87ec37f2fb12dc1ba4ef6ec07fcc
azreal1231 <52235517+azreal1231@users.noreply.github.com>
2024-02-07 13:24:12 +0200

    ? disclaimer update

--- src/comps/disclaimer.jsx
+++ src/comps/disclaimer.jsx
@@ -5,11 +5,16 @@ return <>
     <div className="col-12 col-lg-8">
         <p className='text-white p-4 pt-0 opacity-50 disclaimer-txt'>
             Please note that all retainer and add-on hour tiers cover a range of services including development work, support, and meetings. 
+            Retainer hours are essentially a reservation of time, and you pay for the reservation of this time, 
+            ensuring dedicated availability for your project needs. 
             The hours purchased within any tier are cumulative and account for the total time spent on all project-related activities, 
             encompassing coding, troubleshooting, as well as consultation and strategy meetings. 
-            We strive to use this time efficiently to maximize the value and progress of your project. 
-            It is our policy to maintain transparency and open communication about the allocation of hours to ensure mutual understanding and satisfaction. 
-            For any specific questions or concerns about how hours are allocated, please feel free to contact us directly.
+            This approach allows us to use the allocated time efficiently, maximizing the value and progress of your project. 
+            We are committed to maintaining transparency and open communication about the allocation of hours to ensure mutual understanding and satisfaction. 
+            Retainer hours guarantee that we reserve specific amounts of time exclusively for your project, 
+            emphasizing our commitment to its success. 
+            For any specific questions or concerns about how hours are allocated or the reservation of time through retainers, 
+            please feel free to contact us directly.
         </p>
     </div>
 </div>
